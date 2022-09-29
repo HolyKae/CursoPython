d5982d83dc0100d593fbeb2deee2a9603f90b30b
HolyKae <114181181+HolyKae@users.noreply.github.com>
2022-09-29 15:22:25 -0300

    Curso de Python

--- Curso/ex55.py
+++ Curso/ex55.py
@@ -2,8 +2,12 @@ g = 0
 f = 1000
 for c in range(1, 6):
     p = float(input(f'Digite o peso da {c}ª pessoa: '))
-    if p < f:
+    if p == 1:
         f = p
-    elif p > g:
         g = p
+    else:
+        if p < f:
+            f = p
+        if p > g:
+            g = p
 print(f'A pessoa mais pesada pesa {g:.2f}Kg e a mais leve pesa {f:.2f}Kg')
