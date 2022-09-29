ce6e9a3d819ae3d0efbb5a1e2726ab5ab263b62d
HolyKae <114181181+HolyKae@users.noreply.github.com>
2022-09-29 13:33:15 -0300

    Curso de Python

--- Curso/ex53.py
+++ Curso/ex53.py
@@ -1,6 +1,14 @@
-f = input('Digite uma frase: ').upper().strip().replace(' ', '')
-r = f[::-1]
-if f == r:
+f = input('Digite uma frase: ').upper().strip()
+p = f.split()
+j = ''.join(p)
+r = ''
+'''Sem for:
+   f = input('Digite uma frase: ').upper().strip().replace(' ', '')
+   r = f[::-1]
+   continua no if'''
+for le in range(len(j) - 1, -1, -1):
+    r += j[le]
+if j == r:
     print('É Palindromo!')
 else:
     print('Não é palindromo.')
