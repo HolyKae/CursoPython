42af5d71882449779ea65a9df20ad8987921ede0
Rodrigo Junior <rasjr2008@hotmail.com>
2022-09-29 21:30:39 -0300

    Curso de Python

--- Curso/ex59.py
+++ Curso/ex59.py
@@ -1,16 +1,15 @@
 n1 = int(input('Digite um número: '))
 n2 = int(input('Digite outro número: '))
-o = int(input('Escolha uma opção:\n1 - [+]\n2 - [*]\n3 - [<>]\n4 - Novos números\n5 - Sair.\n'))
-while o > 5:
+o = 1
+while o > 5 and o == 0:
     print(f'Ta vendo {o} onde aí cabeça de butijão??/???')
     o = int(input('Escolha uma opção:\n1 - [+]\n2 - [*]\n3 - [<>]\n4 - Novos números\n5 - Sair.\n'))
 while o != 5:
+    o = int(input('Escolha uma opção:\n1 - [+]\n2 - [*]\n3 - [<>]\n4 - Novos números\n5 - Sair.\n'))
     if o == 1:
         print(f'{n1} + {n2} = {n1 + n2}')
-        break
     elif o == 2:
         print(f'{n1} * {n2} = {n1 * n2}')
-        break
     elif o == 3:
         if n1 > n2:
             print(f'{n1} > {n2}')
@@ -18,8 +17,6 @@ while o != 5:
             print(f'{n1} < {n2}')
         else:
             print(f'{n1} = {n2}')
-        break
     elif o == 4:
         n1 = int(input('Digite um número: '))
         n2 = int(input('Digite outro número: '))
-        break
