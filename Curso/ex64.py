634b8635371099680b0b0f0680e6df0db3b19334
HolyKae <114181181+HolyKae@users.noreply.github.com>
2022-09-30 08:43:19 -0300

    Curso de Python

--- Curso/ex64.py
+++ Curso/ex64.py
@@ -1,8 +1,7 @@
-f = 999
 s = c = 0
 n = 0
-while f != n:
-    n = int(input('Digite um número: '))
+while 999 != n:
+    n = int(input('Digite um número [999 para parar]: '))
     s += n
     c += 1
 print(f'Foram inseridos {c} números\nA soma deles é {s}')
