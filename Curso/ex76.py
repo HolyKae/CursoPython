7fa2f83429f8274cf48648e8df8b6ab0cd445518
Rodrigo Junior <rasjr2008@hotmail.com>
2022-10-04 19:02:08 -0300

    Curso de Python

--- Curso/ex76.py
+++ Curso/ex76.py
@@ -1,5 +1,5 @@
 g = ('Lapis', 1, 'Borracha', 0.50, 'Caderno', 15, 'Estojo', 25, 'Mochila', 120)
 for i in range(len(g)):
     if i % 2 == 0:
-        print(f'{g[i]:}', end=('.' * (20 - len(g[i]))))
+        print(f'{g[i]:.<30}', end='')
         print(f'R${g[i+1]:7.2f}')
