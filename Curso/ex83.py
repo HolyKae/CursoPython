d0f87dd0f857a95a835bda0bcfd617fa5eef45b0
HolyKae <114181181+HolyKae@users.noreply.github.com>
2022-10-06 10:51:30 -0300

    Curso de Python

--- Curso/ex83.py
+++ Curso/ex83.py
@@ -1,8 +1,5 @@
-e = input('Digite uma expressão: ')
-li = list(e)
-print(li)
-print(li.count('('))
-if li.count('(') == li.count(')'):
+e = list(input('Digite uma expressão: '))
+if e.count('(') == e.count(')'):
     print('A expressão é válida.')
 else:
     print('A lista é inválida.')
