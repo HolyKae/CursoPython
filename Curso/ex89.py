241717a7210bd34861f0fbfb20b4e9cf0ac8b205
Rodrigo Junior <rasjr2008@hotmail.com>
2022-10-06 22:27:40 -0300

    Curso de Python

--- Curso/ex89.py
+++ Curso/ex89.py
@@ -14,28 +14,28 @@ while True:
         al.append(p[:])
     else:
         break
+print('--' * 50)
+for i in range(len(al)):
+    print(
+        f'Número: {i + 1} -+- '
+        f'Nome: {al[i][0][0]:.<20} -+-+-+- '
+        f'Notas: 1ª- {al[i][1][0]:2.1f} -+- 2ª- {al[i][1][1]:2.1f} '
+        f'-+-+-+- Média: {al[i][2][0]:2.1f}')
+    print('--' * 50)
 while True:
     d = input('Quer escolher um aluno especifico? [S/N]').upper()
     if d not in 'SN':
         print('buro')
         d = input('Quer escolher um aluno especifico? [S/N]').upper()
     elif d in 'S':
-        d = input('Digite o nome do aluno: ')
+        d = int(input('Digite o número do aluno: '))
         for i in range(len(al)):
-            if d in al[i][0]:
+            if (i + 1) == d:
                 print('--' * 50)
                 print(f'Nome: {al[i][0][0]:.<20} -+-+-+- '
                       f'Notas: 1ª- {al[i][1][0]:2.1f} -+- 2ª- {al[i][1][1]:2.1f} '
                       f'-+-+-+- Média: {al[i][2][0]:2.1f}')
                 print('--' * 50)
-        break
     else:
-        print('--' * 50)
-        for i in range(len(al)):
-            print(
-                f'Nome: {al[i][0][0]:.<20} -+-+-+- '
-                f'Notas: 1ª- {al[i][1][0]:2.1f} -+- 2ª- {al[i][1][1]:2.1f} '
-                f'-+-+-+- Média: {al[i][2][0]:2.1f}')
-            print('--' * 50)
         break
 
