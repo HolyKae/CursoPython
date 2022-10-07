6a1bb57f37a83e56fda1122261156a9333026cc5
HolyKae <114181181+HolyKae@users.noreply.github.com>
2022-10-07 10:26:32 -0300

    Curso de Python

--- Curso/ex86.py
+++ Curso/ex86.py
@@ -1,22 +1,10 @@
 n = [[], [], []]
-lin = 0
-col = 0
-for c in range(9):
-    num = int(input(f'Digite um valor para [{col}][{lin}]: '))
-    n[col].append(num)
-    lin += 1
-    if lin == 3:
-        lin = 0
-    if (c + 1) % 3 == 0:
-        col += 1
-lin = 0
-col = 0
-for c in range(9):
-    print(f'[{n[col][lin]:^5}]', end='   ')
-    lin += 1
-    if lin == 3:
-        lin = 0
-        print()
-        print()
-    if (c + 1) % 3 == 0:
-        col += 1
+for l in range(3):
+    for c in range(3):
+        num = int(input(f'Digite um valor para [{l}][{c}]: '))
+        n[l].append(num)
+for l in range(3):
+    for c in range(3):
+        print(f'[{n[l][c]:^5}]', end='   ')
+    print()
+
--- Curso/ex87.py
+++ Curso/ex87.py
@@ -1,30 +1,17 @@
 n = [[], [], []]
-lin = 0
-col = 0
 par = 0
-ma2 = 0
 som = 0
-for c in range(9):
-    num = int(input(f'Digite um valor para [{col}][{lin}]: '))
-    n[col].append(num)
-    if n[col][lin] % 2 == 0:
-        par += n[col][lin]
-    lin += 1
-    if lin == 3:
-        som += num
-        lin = 0
-    if (c + 1) % 3 == 0:
-        col += 1
-lin = 0
-col = 0
-for c in range(9):
-    print(f'[{n[col][lin]:^5}]', end='   ')
-    lin += 1
-    if lin == 3:
-        lin = 0
-        print()
-        print()
-    if (c + 1) % 3 == 0:
-        col += 1
+for l in range(3):
+    for c in range(3):
+        num = int(input(f'Digite um valor para [{l}][{c}]: '))
+        n[l].append(num)
+        if n[l][c] % 2 == 0:
+            par += n[l][c]
+        if c == 2:
+            som += num
+for l in range(3):
+    for c in range(3):
+        print(f'[{n[l][c]:^5}]', end='   ')
+    print()
 n[1].sort()
 print(f'A soma da 3º coluna é: {som}\nA soma dos números pares é: {par}\nO maior número da segunda linha é: {n[1][-1]}')
